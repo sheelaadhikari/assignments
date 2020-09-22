43131bce7881f09879bdd88e83f9a2a383d18bac
Sheela Adhikari <adhikarisheela1000@gmail.com>
2020-09-22 11:51:56 +0545

    operators

---
+++ operators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    int a, b, max;
+    printf(" enter values for a and b: ");
+    scanf("%d%d",&a,&b);
+    max=a>b? a:b;
+    printf("larger of %d and %d is %d \n",a,b,max);
+
+}
+
+
