8bf7d0cb0779a77f9d256c91f7b0274d4c1ee0c7
Sheela Adhikari <adhikarisheela1000@gmail.com>
2020-09-27 07:53:38 +0545

    use of if.. else

---
+++ else.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main(){
+    int a,b;
+    printf("enter a number: ");
+    scanf("%d",&a);
+    printf("enter a number: ");
+    scanf("%d",&b);
+    if(a>b)
+        printf("larger number =%d and smaller number =%d\n",a,b);
+    
+        else
+        printf("larger number= %d and smaller number= %d",b,a);
+}
+
+
+        
+        
+        
+        
+    
+
