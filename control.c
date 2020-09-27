02fb71b98300bee8e9fac028d292e66ec9259f9c
Sheela Adhikari <adhikarisheela1000@gmail.com>
2020-09-27 07:43:08 +0545

    use of negative number

---
+++ control.c
@@ -0,0 +1,11 @@
+// program to print  the message if negative number is entere.
+#include <stdio.h>
+void main(){
+    int num;
+    printf("enter a number: ");
+    scanf("%d",&num);
+    if(num<0);
+    printf("entered number is negative\n");
+    printf("the number you entered is %d",num);
+}
+
