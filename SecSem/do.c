175ca5ad16968477339b227591ff3d58143047d3
Sheela Adhikari <adhikarisheela1000@gmail.com>
2020-12-08 15:26:56 +0545

    prime number

---
+++ SecSem/do.c
@@ -0,0 +1,11 @@
+// write a program to print 
+#include <stdio.h>
+void main(){
+    int choice;
+    printf("enter your choice: ");
+    scanf("%d",choice);
+    switch(choice){
+        
+    }
+
+}
