0bf9d9172c3e422b3ccd25e7b0888cd9354aa448
Sheela Adhikari <adhikarisheela1000@gmail.com>
2020-12-09 08:47:59 +0545

    array

--- SecSem/do.c
+++ SecSem/do.c
@@ -1,11 +1,19 @@
-// write a program to print 
+// write a program to print
 #include <stdio.h>
-void main(){
-    int choice;
-    printf("enter your choice: ");
-    scanf("%d",choice);
-    switch(choice){
-        
+void main()
+{
+    int a, b, x, i;
+    printf("enter the two number: ");
+    scanf("%d%d", &a, &b);
+    for (x = a + 1; x <= b - 1; i++)
+    {
+        for (i = 2; i < x; i++)
+        {
+            break;
+        }
+        if (i == x)
+        {
+            printf("%d\t", x);
+        }
     }
-
 }
