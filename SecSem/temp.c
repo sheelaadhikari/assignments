4caf20f1168484bd9b66029660e330e68c37b2e4
Sheela Adhikari <adhikarisheela1000@gmail.com>
2021-02-17 19:02:12 +0545

    temperature

---
+++ SecSem/temp.c
@@ -0,0 +1,15 @@
+// wap to find the lowest and highest temperature of the 75 district.
+#include <stdio.h>
+void main()
+{
+    int i, temp, low = 1000;
+    for (i = 1; i <= 75; i++)
+    {
+        printf("enter the temp: ");
+        scanf("%d", &temp);
+        if (temp < low)
+            low = temp;
+    }
+
+    printf("the lowest temperature is %d", low);
+}
