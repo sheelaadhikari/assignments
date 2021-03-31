973ba624f007f954305c5661b2080ece5f5dd0c2
Sheela Adhikari <adhikarisheela1000@gmail.com>
2021-03-31 17:23:14 +0545

    prime using function

--- Functions/prime.c
+++ Functions/prime.c
@@ -1,24 +1,25 @@
 //write a program to test the number is prime or not
 #include<stdio.h>
 #include<math.h>
-int prime(int n);
+float prime(int n);
 main(){
-    int n;
+    int num;
     printf("enter the number: ");
-    scanf("%d", &n);
-     if(prime(n))
+    scanf("%d", &num);
+     if(prime(num))
     
     printf("the number is  prime/n");
     else
     printf("the given number not prime");
     
 }
-int prime(int n){
-    int num, flag=1;
-   for(num=0; num<=sqrt(n); num++){
-     if(n%num==0){
+float prime(int n){
+    int i, flag=1;
+   for(i=0; i<=n*n ; i++){
+     if(n%i==0){
          flag=0;
          break;
     } 
+   }
     return (flag);
 }
