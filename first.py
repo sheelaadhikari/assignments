0a8145a4fef3245f6d9acb372a5d4d47eb7a87b5
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2021-04-27 21:52:54 +0545

    Add files via upload

---
+++ first.py
@@ -0,0 +1,66 @@
+# #  # # print("Hello world")
+# # # # x = 10
+# # # # y = 11
+# # # # print(x+y)
+# # # # print("bhola adhikari")
+# # # # name=input("what is your name")
+# # # # favourite_colour=input("what is your favourite colour?")
+# # # # print(name+'likes'+favourite_
+# # # length_in_m=input("length: ")
+# # # length_in_cm=100*int(length_in_m)
+# # # print(length_in_cm)
+# # # print(
+# # #     """hello
+# # # how are you"""
+# # # )
+# # # subject = "englist is easiest subject"
+# # # print(subject[0:5])
+# # # first = "steve"
+# # # last = "smith"
+# # # message = first + "[" + last + "] is a crickter"
+# # # print(message)
+# # # msg = f"{first} [{last}] is a cricketer"
+# # # topic= "apple is a fruit"
+# # # print(len(topic))
+# # # print(topic.upper())
+# # # print(topic.find("a"))
+# # # topic.replace("apple","mango")
+# # # x= 20 //5
+# # # print(x+2)
+# # # y= 5
+# # # y-=2
+# # # print(x)
+# # # p=-3.1
+# # # print(abs(p))
+# # # import math
+# # # print(math.floor(3.6))
+# # age= input("what is your age?")
+# # if int(age)<=2020:
+# #     print("you are a child")
+# # elif 20<int(age)<60:
+# #     print("you are adult") 
+# # else:
+# #     print("you are old")
+# have_books= True
+# have_dress= False
+# if have_books or have_dress:
+#     print("you can go to school")
+# else:
+#     print("you can not go to school")  
+
+# price= int(input("what is the price of the bike"))
+# brand= input("which is the brand")
+# if price<10000 and brand=="bajaj":
+#     print("i will buy it")
+#     print("this bike suits me")
+# else:
+#     print("i will not buy it")    
+
+# weight=int(input('what is your weight:' ))
+# unit=input("kg or g: " )
+# if unit=="kg":
+#     converted=weight*1000
+#     print(f"you are{converted} g ")
+# else:
+#     converted=weight//1000
+#     print(f"you are{converted} kg")
