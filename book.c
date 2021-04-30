6b0c1647d21f464b4271b0535f904fce6391c9b3
sheelaadhikari <adhikarisheela1000@gmail.com>
2021-04-30 09:33:40 +0545

    cc

--- book.c
+++ book.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main(){
     int a=5, b=5;
-    printf("the sum is %d\n", a+b);
-    printf("the subtraction is %d\n", a-b);
-    printf("the mult is %d\n", a*b);
+    printf("the sum is %d", a+b);
+    printf("the subtraction is %d", a-b);
+    printf("the mult is %d", a*b);
 }
---
+++ programmingall.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main(){
+    float a=5.5, b=5.5;
+    printf("sum is %2f\n", a+b);
+    printf("diff is %2f\n", a-b);
+}
