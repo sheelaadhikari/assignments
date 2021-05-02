b0d933cb656f1395c16f82a7b1ba77dba2a0f5c6
sheelaadhikari <adhikarisheela1000@gmail.com>
2021-05-02 07:58:14 +0545

    cc

--- programmingall.c
+++ programmingall.c
@@ -1,8 +1,25 @@
-#include<stdio.h>
-void main(){
+//#include<stdio.h>
+//void main(){
     //float a=5.5, b=5.5;
     //printf("sum is %2f\n", a+b);
     //printf("diff is %2f\n", a-b);
-    int x=7;
-    printf("x=%d and x=%d", ++x, --x);
-}
+    //int x=7;
+   // printf("x=%d and x=%d", ++x, --x);
+//}
+//int a, b, max;
+//printf("enter the values :");
+//scanf("%d %d",&a, &b);
+//max=a>b? a:b;
+//printf("the larger lumber is %d\n", max);
+//}
+
+//# include<stdio.h>
+//v//oid main(){
+    //int num;
+    //printf("enter any number: ");
+    //scanf("%d", &num);
+    //if(num<0)
+    //printf("the number you entered is negative");
+    //else
+    //printf("error");
+//}
