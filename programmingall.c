5cacc9a16a80111e42d7290a89df3bc3e78abdd8
sheelaadhikari <adhikarisheela1000@gmail.com>
2021-05-02 07:01:53 +0545

    cc

--- programmingall.c
+++ programmingall.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 void main(){
-    float a=5.5, b=5.5;
-    printf("sum is %2f\n", a+b);
-    printf("diff is %2f\n", a-b);
+    //float a=5.5, b=5.5;
+    //printf("sum is %2f\n", a+b);
+    //printf("diff is %2f\n", a-b);
+    int x=7;
+    printf("x=%d and x=%d", ++x, --x);
 }
