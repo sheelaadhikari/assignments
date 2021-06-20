9c0ecfe2caa4e5606d79089f1c3aa41201240e8b
Sheela Adhikari <adhikarisheela1000@gmail.com>
2021-06-20 19:11:57 +0545

    string length

---
+++ Project/task5.c
@@ -0,0 +1,19 @@
+//  find string length
+#include <stdio.h>
+#include <string.h>
+
+// int main()
+// {
+
+char str(char[100]);
+main()
+{
+    char Str[100] = "sheela";
+    int Length;
+
+    Length = strlen(Str);
+
+    printf("Length of a String = %d\n", Length);
+
+    return 0;
+}
