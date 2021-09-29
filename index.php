1bf27a2d5678d583f99a04646b987474975b7305
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2021-09-29 17:25:18 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$host = "mariadb";
+$user = "root";
+$pass = "rootpwd";
+$db = "app1";
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
+?>
